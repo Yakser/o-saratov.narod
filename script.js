cf062edb70dd792b48e97ac524fa848dc387911a
Sergey Yaksanov <53031664+Yakser@users.noreply.github.com>
2020-08-26 18:20:59 +0400

    Add files via upload
    add a new favicon

--- script.js
+++ script.js
@@ -34,9 +34,13 @@ let upButton = document.querySelector('.up-button');
 window.onscroll = function () {
   if (window.pageYOffset > 200) {
     upButton.classList.add('shown');
+    
+
   } else {
     upButton.classList.remove('shown');
+     
   }
+
 
 };
 
